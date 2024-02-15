99b9d2b7659850cf4e2b1d979eb64777c0ed32f6
janithwijethunga <janithwijethunga68@gmail.com>
2024-02-15 18:34:06 +0530

    create cars model

--- models/cars.js
+++ models/cars.js
@@ -1,3 +1,44 @@
 const mongoose = require("mongoose")
 
-const roomSchema = mongoose.Schema 
+const carSchema = mongoose.Schema ({
+    name: {
+        type: String ,
+        required: true
+    },
+
+    maxcount: {
+        type: Number ,
+        required: true
+    },
+
+    phone_number: {
+        type: number ,
+        required: true
+    },
+
+    rentperday: {
+        type: number ,
+        required: true
+    },
+
+    imageurls: [],
+    current_bookings : [],
+    type: {
+        type: String ,
+        required: true
+    },
+
+    description : {
+        type: String, 
+        required: true
+    }
+
+},{
+    timestamps: true,
+})
+
+const carModel = mongoose.model('cars', carSchema)
+
+module.exports = carModel
+
+
